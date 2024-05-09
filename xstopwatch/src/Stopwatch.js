b6fa9343e3a56a333f6bcfc06308a72f4db7a790
Lakshmi priya <soundcuttie@gmail.com>
2024-05-09 17:25:54 +0530

    xstopwatch commit

---
+++ xstopwatch/src/Stopwatch.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+
+
+
+
+
+export default function Stopwatch() {
+
+    const [timeron , setTimeron] = useState(0)
+    const [timeronflag , setTimeronflag] = useState(false)
+
+    const formatTime = (seconds) => {
+        const min = Math.floor(seconds / 60);
+        const remainingsec = seconds % 60;
+        return `${min}:${remainingsec < 10 ? "0" : ""}${remainingsec}`;
+    }
+
+    const handleStart = () => {
+     
+        setTimeronflag((prev) => !prev)
+       
+    }
+
+    const handleReset = () => {
+        setTimeron(0);
+        setTimeronflag(false)
+    }
+
+    useEffect(() => {
+        let timer;
+        if(timeronflag){
+            timer = setInterval(() => {
+                setTimeron((prev) => prev+1)
+            }, 1000);
+        }
+        // else{
+        //     clearInterval(timer)
+        // }
+
+        return () => clearInterval(timer)
+    },[timeronflag])
+
+        return(
+            <div>
+                <h2>Stopwatch</h2>
+                <h4>Time : {formatTime(timeron)}</h4>
+                <button onClick={handleStart}>
+                {timeronflag ? 'stop' : 'start'}
+                    
+                    </button>
+                <button onClick={handleReset}>Reset</button>
+            </div>
+        )
+  }
